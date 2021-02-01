cc13471128d3cc628d1aa006d518762019f495d0
Inwerwm <saclainvervm@gmail.com>
2021-02-01 15:06:14 +0900

    全フレームの取得をプロパティからに置換

--- VMDInverter/VMDInverter.cs
+++ VMDInverter/VMDInverter.cs
@@ -11,13 +11,7 @@ namespace VMDInverter
     {
         public static void Do(VocaloidMotionData vmd)
         {
-            List<IVocaloidFrame> allFrames = new();
-            allFrames.AddRange(vmd.CameraFrames);
-            allFrames.AddRange(vmd.LightFrames);
-            allFrames.AddRange(vmd.MorphFrames);
-            allFrames.AddRange(vmd.MotionFrames);
-            allFrames.AddRange(vmd.PropertyFrames);
-            allFrames.AddRange(vmd.ShadowFrames);
+            List<IVocaloidFrame> allFrames = vmd.Frames;
 
             var maxTime = allFrames.Max(frame => frame.Frame);
 
